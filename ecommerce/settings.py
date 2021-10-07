c2bc168a8e1ffe48f7a70a0af6e8e8d699c6e46a
Elviin Thomas Eldho <elviin.t.eldho@gmail.com>
2021-10-07 18:04:32 +0530

    Allowed Hosts Updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = os.getenv("DEBUG", "False") == "True"
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['atb-online.herokuapp.com','127.0.0.1']
 
 # Application definition
 
